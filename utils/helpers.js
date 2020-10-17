b43727b302389f095e1fd6199f7f4035225b3d55
fervailanti <fernando.vailanti@iunigo.com>
2020-10-17 07:11:43 -0300

    spli price in amount and decimals

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,33 +1,46 @@
 export const autor = { name: 'Fernando', lastname: 'Vailanti' }
 
-export const createFullItem = (item) => ({
-  id: item.id,
-  title: item.title,
-  price: {
-    currency: item.currency_id,
-    amount: item.price,
-    decimals: 0
-  },
-  picture: item.pictures[0].secure_url,
-  condition: item.condition,
-  free_shipping: item.shipping.free_shipping,
-  sold_quantity: item.sold_quantity
-})
+const splitPrice = (price) =>
+  price
+    .toString()
+    .split('.')
+    .map((str) => parseInt(str))
 
-export const createItemsList = (results) => {
-  const formattedList = results.map((item) => ({
+export const createFullItem = (item) => {
+  const [amount, decimals] = splitPrice(item.price)
+  return {
     id: item.id,
     title: item.title,
     price: {
       currency: item.currency_id,
-      amount: item.price,
-      decimals: 0
+      amount,
+      decimals
     },
-    picture: item.thumbnail,
+    picture: item.pictures[0].secure_url,
     condition: item.condition,
-    free_shipping: item.shipping.free_shipping
-  }))
-  return formattedList.slice(0, 4)
+    free_shipping: item.shipping.free_shipping,
+    sold_quantity: item.sold_quantity
+  }
+}
+
+export const createItemsList = (results) => {
+  return results
+    .map((item) => {
+      const [amount, decimals] = splitPrice(item.price)
+      return {
+        id: item.id,
+        title: item.title,
+        price: {
+          currency: item.currency_id,
+          amount,
+          decimals
+        },
+        picture: item.thumbnail,
+        condition: item.condition,
+        free_shipping: item.shipping.free_shipping
+      }
+    })
+    .slice(0, 4)
 }
 
 export const createBreadcrumb = (filters) => {
